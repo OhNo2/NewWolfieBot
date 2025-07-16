5658f6bd94fc0eaf94cf7bb2edac64876c07b76c
OhNo2 <42597581+OhNo2@users.noreply.github.com>
2025-07-16 00:20:48 -0400

    Update bot.py
    updated form version for this year

--- bot.py
+++ bot.py
@@ -575,7 +575,7 @@ class gcal:
                             email_date = dtdate.strftime("%Y-%m-%d")
                             nl = '\n'
                             event_reference = str(sheet.cell(f"BH{shrow}").value)
-                            form_link=f"https://docs.google.com/forms/d/e/1FAIpQLSflDZ8AjHxNqTEyk3-GAnLdlabZ7WlPsns4SF8y4TuChLgTwA/viewform?usp=pp_url&entry.708486352={event_name.replace(' ','%20')}&entry.1669706537={email_date}&entry.395191729={event_reference}"
+                            form_link=f"https://docs.google.com/forms/d/e/1FAIpQLSdYB7Jc-cGcEVAMkK6kfEo1S5QAb1GxFsJTvolY6_3UexvaqQ/viewform?usp=pp_url&entry.708486352={event_name.replace(' ','%20')}&entry.1669706537={email_date}&entry.395191729={event_reference}"
                             body = f"Hello {requester_name},%0D%0A%0D%0AWolfie had a lot of fun at {event_name}! If you have any events in the future that you would like Wolfie to attend, please put in a request (link in my signature).%0D%0A%0D%0AWe are always looking to improve how Wolfie does at events, so I was hoping you could fill out a brief survey on how Wolfie did! Any comments help us to make events better in the future! Please take the time to fill out this survey:%0D%0A%0D%0A{form_link}"
                             body = body.replace("%0D%0A",nl)
                             email_link = f"{body}"
